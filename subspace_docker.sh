04d8905fde4eef6ec5ca4cd1668fcaca0661228e
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-04-06 15:56:23 +0300

    try

--- subspace_docker.sh
+++ subspace_docker.sh
@@ -10,24 +10,16 @@ while test $# -gt 0; do
             function="install"
             shift
             ;;
-        -in2|--second)
-            function="second"
+        -ml|--multi)
+            function="multi"
             shift
-            ;;
-        -in3|--three)
-            function="three"
-            shift
-            ;;    
+            ;;   
         -un|--uninstall)
             function="uninstall"
             shift
             ;;
-        -un2|--uninstall2)
-            function="uninstall2"
-            shift
-            ;;
-        -un3|--uninstall3)
-            function="uninstall3"
+        -una|--uninstallall)
+            function="uninstallall"
             shift
             ;;
         *|--)
@@ -150,178 +142,99 @@ sleep 2
 docker compose up -d && docker compose logs -f --tail 1000
 
 }
-second() {
+multi() {
 cd $HOME
-#create var2
-#SUBSPACE_WALLET_ADDRESS2
-if [ ! $SUBSPACE_WALLET_ADDRESS2 ]; then
-		read -p "Enter wallet address2: " SUBSPACE_WALLET_ADDRESS2
-		echo 'export SUBSPACE_WALLET_ADDRESS2='${SUBSPACE_WALLET_ADDRESS2} >> $HOME/.bash_profile
-	fi
-#SUBSPACE_NODE_NAME2
-if [ ! $SUBSPACE_NODE_NAME2 ]; then
-		read -p "Enter node name2: " SUBSPACE_NODE_NAME2
-		echo 'export SUBSPACE_NODE_NAME2='$SUBSPACE_NODE_NAME2 >> $HOME/.bash_profile
-	fi
-#SUBSPACE_PLOT_SIZE
-if [ ! $SUBSPACE_PLOT_SIZE2 ]; then
-		read -p "Enter plot size 50-100G: " SUBSPACE_PLOT_SIZE2
-		echo 'export SUBSPACE_PLOT_SIZE2='$SUBSPACE_PLOT_SIZE2 >> $HOME/.bash_profile
-	fi
-#version
-#local subspace_version=`wget -qO- https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name"`
-#create dir and config
-if [ ! -d $HOME/subspace2 ]; then
-mkdir $HOME/subspace2
-fi
-cd $HOME/subspace2
-sleep 1
- # Create script 
- tee $HOME/subspace2/docker-compose.yml > /dev/null <<EOF
-  version: "3.7"
-  services:
-    node:
-      image: ghcr.io/subspace/node:$version
-      volumes:
-        - node-data:/var/subspace:rw
-      ports:
-        - "0.0.0.0:34333:34333"
-        - "0.0.0.0:34433:34433"
-      restart: unless-stopped
-      command: [
-        "--chain", "gemini-3c",
-        "--base-path", "/var/subspace",
-        "--execution", "wasm",
-        "--blocks-pruning", "archive",
-        "--state-pruning", "archive",
-        "--port", "34333",
-        "--dsn-listen-on", "/ip4/0.0.0.0/tcp/34433",
-        "--rpc-cors", "all",
-        "--rpc-methods", "safe",
-        "--unsafe-ws-external",
-        "--dsn-disable-private-ips",
-        "--no-private-ipv4",
-        "--validator",
-        "--name", "$SUBSPACE_NODE_NAME2"
-      ]
-      healthcheck:
-        timeout: 5s
-        interval: 30s
-        retries: 5
-
-    farmer:
-      depends_on:
+#create config multi
+		read -p "Enter quantity: " MNODE
+		echo 'export MNODE='$MNODE 
+while [ $MNODE -gt 0 ]
+do
+    #SUBSPACE_WALLET_ADDRESS
+    if [ ! $SUBSPACE_WALLET_ADDRESS${MNODE} ]; then
+		read -p "Enter wallet address${MNODE}: " SUBSPACE_WALLET_ADDRESS${MNODE}
+		echo 'export SUBSPACE_WALLET_ADDRESS${MNODE}='${SUBSPACE_WALLET_ADDRESS${MNODE}} >> $HOME/.bash_profile
+	  fi
+    #SUBSPACE_NODE_NAME
+    if [ ! $SUBSPACE_NODE_NAME${MNODE} ]; then
+		read -p "Enter node name${MNODE}: " SUBSPACE_NODE_NAME${MNODE}
+		echo 'export SUBSPACE_NODE_NAME${MNODE}='$SUBSPACE_NODE_NAME${MNODE} >> $HOME/.bash_profile
+	  fi
+    #SUBSPACE_PLOT_SIZE
+    if [ ! $SUBSPACE_PLOT_SIZE${MNODE} ]; then
+		read -p "Enter plot size 50-100G: " SUBSPACE_PLOT_SIZE${MNODE}
+		echo 'export SUBSPACE_PLOT_SIZE${MNODE}='$SUBSPACE_PLOT_SIZE${MNODE} >> $HOME/.bash_profile
+	  fi
+    #version
+    #local subspace_version=`wget -qO- https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name"`
+    #create dir and config
+    if [ ! -d $HOME/subspace${MNODE} ]; then
+    mkdir $HOME/subspace${MNODE}
+    fi
+    cd $HOME/subspace${MNODE}
+    sleep 1
+    # Create script 
+     tee $HOME/subspace${MNODE}/docker-compose.yml > /dev/null <<EOF
+     version: "3.7"
+      services:
         node:
-          condition: service_healthy
-      image: ghcr.io/subspace/farmer:$version
-      volumes:
-        - farmer-data:/var/subspace:rw
-      ports:
-        - "0.0.0.0:34533:34533"
-      restart: unless-stopped
-      command: [
-        "--base-path", "/var/subspace",
-        "farm",
-        "--disable-private-ips",
-        "--node-rpc-url", "ws://node:9944",
-        "--listen-on", "/ip4/0.0.0.0/tcp/34533",
-        "--reward-address", "$SUBSPACE_WALLET_ADDRESS2",
-        "--plot-size", "$SUBSPACE_PLOT_SIZE2"
-      ]
-  volumes:
-    node-data:
-    farmer-data:
-EOF
-sleep 2
-#docker run
-docker compose up -d && docker compose logs -f --tail 1000
-}
-three() {
-cd $HOME
-#create var3
-#SUBSPACE_WALLET_ADDRESS3
-if [ ! $SUBSPACE_WALLET_ADDRESS3 ]; then
-		read -p "Enter wallet address3: " SUBSPACE_WALLET_ADDRESS3
-		echo 'export SUBSPACE_WALLET_ADDRESS3='${SUBSPACE_WALLET_ADDRESS3} >> $HOME/.bash_profile
-	fi
-#SUBSPACE_NODE_NAME3
-if [ ! $SUBSPACE_NODE_NAME3 ]; then
-		read -p "Enter node name3: " SUBSPACE_NODE_NAME3
-		echo 'export SUBSPACE_NODE_NAME3='$SUBSPACE_NODE_NAME3 >> $HOME/.bash_profile
-	fi
-#SUBSPACE_PLOT_SIZE3
-if [ ! $SUBSPACE_PLOT_SIZE3 ]; then
-		read -p "Enter plot size 50-100G: " SUBSPACE_PLOT_SIZE3
-		echo 'export SUBSPACE_PLOT_SIZE3='$SUBSPACE_PLOT_SIZE3 >> $HOME/.bash_profile
-	fi
-#version
-#local subspace_version=`wget -qO- https://api.github.com/repos/subspace/subspace/releases/latest | jq -r ".tag_name"`
-#create dir and config
-if [ ! -d $HOME/subspace3 ]; then
-mkdir $HOME/subspace3
-fi
-cd $HOME/subspace3
-sleep 1
- # Create script 
- tee $HOME/subspace3/docker-compose.yml > /dev/null <<EOF
-  version: "3.7"
-  services:
-    node:
-      image: ghcr.io/subspace/node:$version
-      volumes:
-        - node-data:/var/subspace:rw
-      ports:
-        - "0.0.0.0:35333:35333"
-        - "0.0.0.0:35433:35433"
-      restart: unless-stopped
-      command: [
-        "--chain", "gemini-3c",
-        "--base-path", "/var/subspace",
-        "--execution", "wasm",
-        "--blocks-pruning", "archive",
-        "--state-pruning", "archive",
-        "--port", "35333",
-        "--dsn-listen-on", "/ip4/0.0.0.0/tcp/35433",
-        "--rpc-cors", "all",
-        "--rpc-methods", "safe",
-        "--unsafe-ws-external",
-        "--dsn-disable-private-ips",
-        "--no-private-ipv4",
-        "--validator",
-        "--name", "$SUBSPACE_NODE_NAME3"
-      ]
-      healthcheck:
-        timeout: 5s
-        interval: 30s
-        retries: 5
+          image: ghcr.io/subspace/node:$version
+          volumes:
+            - node-data:/var/subspace:rw
+          ports:
+           - "0.0.0.0:3${MNODE}333:3${MNODE}333"
+           - "0.0.0.0:3${MNODE}433:3${MNODE}433"
+          restart: unless-stopped
+          command: [
+           "--chain", "gemini-3c",
+           "--base-path", "/var/subspace",
+            "--execution", "wasm",
+            "--blocks-pruning", "archive",
+            "--state-pruning", "archive",
+            "--port", "3${MNODE}333",
+            "--dsn-listen-on", "/ip4/0.0.0.0/tcp/34433",
+            "--rpc-cors", "all",
+            "--rpc-methods", "safe",
+            "--unsafe-ws-external",
+            "--dsn-disable-private-ips",
+            "--no-private-ipv4",
+            "--validator",
+            "--name", "$SUBSPACE_NODE_NAME${MNODE}"
+          ]
+          healthcheck:
+          timeout: 5s
+          interval: 30s
+          retries: 5
 
-    farmer:
-      depends_on:
-        node:
-          condition: service_healthy
-      image: ghcr.io/subspace/farmer:$version
-      volumes:
-        - farmer-data:/var/subspace:rw
-      ports:
-        - "0.0.0.0:35533:35533"
-      restart: unless-stopped
-      command: [
-        "--base-path", "/var/subspace",
-        "farm",
-        "--disable-private-ips",
-        "--node-rpc-url", "ws://node:9944",
-        "--listen-on", "/ip4/0.0.0.0/tcp/35533",
-        "--reward-address", "$SUBSPACE_WALLET_ADDRESS3",
-        "--plot-size", "$SUBSPACE_PLOT_SIZE3"
-      ]
-  volumes:
-    node-data:
-    farmer-data:
-EOF
-sleep 2
-#docker run
-docker compose up -d && docker compose logs -f --tail 1000
+          farmer:
+          depends_on:
+           node:
+                condition: service_healthy
+          image: ghcr.io/subspace/farmer:$version
+          volumes:
+            - farmer-data:/var/subspace:rw
+          ports:
+           - "0.0.0.0:3${MNODE}533:3${MNODE}533"
+          restart: unless-stopped
+         command: [
+           "--base-path", "/var/subspace",
+           "farm",
+           "--disable-private-ips",
+           "--node-rpc-url", "ws://node:9944",
+           "--listen-on", "/ip4/0.0.0.0/tcp/3${MNODE}533",
+           "--reward-address", "$SUBSPACE_WALLET_ADDRESS${MNODE}",
+          "--plot-size", "$SUBSPACE_PLOT_SIZE${MNODE}"
+          ]
+       volumes:
+       node-data:
+       farmer-data:
+    EOF
+    sleep 2
+    #docker run
+   docker compose up -d
+MNODE=$[ $MNODE - 1 ]
+done
 }
+
 uninstall() {
 cd $HOME/subspace
 docker compose down -v
