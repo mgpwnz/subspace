5d78f80e65765f9875e963315747859b00537af2
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-04-15 16:12:55 +0300

    test

--- subspace_docker.sh
+++ subspace_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Default variables
-version="gemini-3d-2023-mar-29"
+version="gemini-3d-2023-apr-14"
 function="install"
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
