601bcde4a687e4303fb80794be82ced737bca4e7
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-04-18 14:48:28 +0300

    1

--- subspace_cli.sh
+++ subspace_cli.sh
@@ -31,7 +31,7 @@ sleep 2
 if [ ! -d $HOME/subspace ]; then
 mkdir $HOME/subspace
 else
-echo -e "Your subspace node installed \e[32m for  update use -up \e[39m!"
+echo -e "Your subspace node installed, for update use \e[32m -up \e[39m!"
 break
 fi
 cd $HOME/subspace
