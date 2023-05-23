f61d7c1a6b9a0638ad80b109756d6ad667c867c4
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-05-23 18:15:26 +0300

    ress

--- subspace_up.sh
+++ subspace_up.sh
@@ -7,9 +7,7 @@ docker compose up -d
 read -r -p "Do you have errors? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-        docker compose down
-        docker compose pull
-        docker compose up -d
+        echo reinstall
         ;;
     *)
         docker compose logs -f
