8dbd20c54d524d85438b3695033e2bd3a9f8bf47
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-05-23 17:23:39 +0300

    test

--- subspace_up.sh
+++ subspace_up.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 while true
 do
-read -r -p "Do you have erros? [y/N] " response
+docker compose down
+docker compose pull
+docker compose up -d
+read -r -p "Do you have errors? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         docker compose down
