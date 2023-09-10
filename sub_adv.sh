19d2ba0a846bddb5a8bf9d1f9219ce0b75b26977
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-10 14:17:24 +0300

    update

--- sub_adv.sh
+++ sub_adv.sh
@@ -72,33 +72,7 @@ WantedBy=multi-user.target
     echo "[Unit]
 Description=Subspace Farmer
 
-while true
-do
-read -r -p "Plot size? [100G/200G/300G/400G/500G] " response
-case "$response" in
-    [100G][100][100g])
-plot={path=/root/subspace_adv,size=100GiB}
-        echo -e "100G"
-        ;;
-    [200G][200][200g]) 
-plot={path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB}
-        echo -e "200G"
-        ;;
-    [300G][300][300g]) 
-plot={path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB}  
-        echo -e "300G"
-        ;;
-    [400G][400][400g]) 
-plot={path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB}    
-        echo -e "400G"
-        ;;
-    [500G][500][500g]) 
-plot={path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB path=/root/subspace_adv,size=100GiB}    
-        echo -e "500G"
-        ;;
-esac
-done
-
+. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/subspace/main/plot.sh)
     [Service]
 User=$USER
 ExecStart=subspace-farmer farm --reward-address $ADDRESS $plot
