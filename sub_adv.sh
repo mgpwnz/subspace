b204cf7da6f3c8bae6ab1ef485200295b96ff289
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-10 14:29:05 +0300

    update 4

--- sub_adv.sh
+++ sub_adv.sh
@@ -109,7 +109,8 @@ sudo systemctl stop subspace-farmer.service
 sudo systemctl stop subspace-node.service
 sudo systemctl disable subspace-farmer.service
 sudo systemctl disable subspace-node.service
-sudo rm /usr/local/bin/subspace-farmer /usr/local/bin/subspace-node $HOME/subspace_adv
+sudo rm /usr/local/bin/subspace-farmer /usr/local/bin/subspace-node 
+sudo rm -rf $HOME/subspace_adv
 echo "Done"
 cd $HOME
 }
