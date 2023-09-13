814ce41ce0f0ef19de373038a7d009e1b170059b
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-13 17:51:52 +0300

    update

--- update.sh
+++ update.sh
@@ -2,15 +2,15 @@
 version=gemini-3f-2023-sep-11
 repo=v0.6.7-alpha
 
-if [ -f /etc/systemd/system/subspace-node.servise ]; then
+if [ -e /etc/systemd/system/subspace-node.servise ]; then
 echo  adv
 fi
 
-if [ -f $HOME/subspace/docker-compose-logs.yml ]; then
+if [ -e $HOME/subspace/docker-compose-logs.yml ]; then
 echo docker
 fi
 
-if [ -f /etc/systemd/system/subspace.servise ]; then
+if [ -e /etc/systemd/system/subspace.service ]; then
 echo Pulsar
 fi
 echo done
