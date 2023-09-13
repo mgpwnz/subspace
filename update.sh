7e9a02c9a7fd5c01f933d75a9393572ed608747d
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-13 18:09:20 +0300

    update

--- update.sh
+++ update.sh
@@ -3,14 +3,41 @@ version=gemini-3f-2023-sep-11
 repo=v0.6.7-alpha
 
 if [ -e /usr/local/bin/subspace-node ]; then
-echo  adv
+echo  SUBSPACE Advanced
+read -r -p "Update node? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        . <(wget -qO- https://raw.githubusercontent.com/mgpwnz/subspace/main/sub_adv.sh) -up
+        ;;
+    *)
+        break
+        ;;
+esac
 fi
 
-if [ -e $HOME/subspace/docker-compose.yml ]; then
-echo docker
+if [ -e /root/subspace/docker-compose.yml ]; then
+echo SUBSPACE DOCKER
+read -r -p "Update node? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        . <(wget -qO- https://raw.githubusercontent.com/mgpwnz/subspace/main/subspace.sh) -up
+        ;;
+    *)
+        break
+        ;;
+esac
 fi
 
 if [ -e /usr/local/bin/pulsar ]; then
-echo Pulsar
+echo SUBSPACE Pulsar
+read -r -p "Update node? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        . <(wget -qO- https://raw.githubusercontent.com/mgpwnz/subspace/main/subspace_cli.sh) -up
+        ;;
+    *)
+        break
+        ;;
+esac
 fi
 echo done
