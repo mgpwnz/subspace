b9282670bb3b50198ea4ce0d2512d1dc79c317f2
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-13 21:07:30 +0300

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-version=gemini-3f-2023-sep-11
-repo=v0.6.7-alpha
+#version=gemini-3f-2023-sep-11
+#repo=v0.6.7-alpha
 
 if [ -e /usr/local/bin/subspace-node ]; then
 echo  SUBSPACE Advanced
