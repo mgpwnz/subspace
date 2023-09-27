5a098b636ad2eaa58bdf7834afd6beb93223fd0c
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-09-27 22:23:01 +0300

    fix_link

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # root
 cd $HOME
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g-1qH4HnvWegoivNxkoEG3O0brsA4UdBmvn' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1g-1qH4HnvWegoivNxkoEG3O0brsA4UdBmvn" -O file.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qH4HnvWegoivNxkoEG3O0brsA4UdBmvn' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qH4HnvWegoivNxkoEG3O0brsA4UdBmvn" -O file.tar.gz && rm -rf /tmp/cookies.txt
 echo Download complite !
 sleep 3
 cd /
