7d397ebede427a3eee30918bde2753362ef539c5
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-12-21 15:02:18 +0200

    update

--- step.sh
+++ step.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-total_steps=10
-current_step=0
+# Default variables
+function="install"
+version=gemini-3g-2023-dec-20
 
-function progress_bar() {
-  local progress=$((current_step * 100 / total_steps))
-  local bar_size=$((progress / 2))
-  local color='\e[32m'  # ANSI-код для зеленого кольору
-  local reset='\e[0m'   # ANSI-код для скидання кольору
+# Options
+option_value() { echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 
-  printf "[${color}%-50s${reset}] ${color}%d%%${reset}\r" $(printf "#%.0s" $(seq 1 $bar_size)) $progress
+# Progress bar function
+progress_bar() {
+  local progress="$1"
+  local length=50
+  local percentage=$((progress * 100 / length))
+  local bar=""
+  local fill_length=$((progress * length / 100))
+
+  for ((i=0; i<fill_length; i++)); do
+    bar+="="
+  done
+
+  printf "[%-50s] %d%%\r" "$bar" "$percentage"
 }
 
-# Ваші команди по крокам
-for ((i=0; i<total_steps; i++)); do
-  # Ваш код для кожного кроку
-  sleep 1  # Приклад затримки
-  ((current_step++))
-  progress_bar
-done
-
-echo -e "\nСкрипт виконано!"
+# Download and execute the script directly with a progress bar
+wget --quiet -O - https://github.com/mgpwnz/subspace/blob/main/sub_adv.sh | {
+  progress=0
+  while read -r line; do
+    progress_bar "$progress"
+    ((progress++))
+  done
+  echo -e "\n"  # Move to the next line after progress bar completion
+} | bash -s -- "$@"
