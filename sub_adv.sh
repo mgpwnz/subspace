6b924cbd6f4c4f95e26ad1d27bb5eb0c181f0bd8
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-01-31 13:40:44 +0200

    gemini-3g-2024-jan-31

--- sub_adv.sh
+++ sub_adv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Default variables
 function="install"
-version=gemini-3g-2024-jan-29-2
+version=gemini-3g-2024-jan-31
 #version=gemini-3g-2024-jan-03
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
