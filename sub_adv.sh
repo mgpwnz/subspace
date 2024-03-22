248ac6314a1049d4109d481312728eb0e7027c96
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-03-22 17:49:15 +0200

    update

--- sub_adv.sh
+++ sub_adv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Default variables
 function="install"
-#new version march 20
-version=gemini-3h-2024-mar-20
+#new version march 22
+version=gemini-3h-2024-mar-22
 # Options
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
